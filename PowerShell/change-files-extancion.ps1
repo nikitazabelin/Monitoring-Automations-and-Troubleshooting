1f9ea10a850fb4176c0eb1de4a605a118e6274f9
Nikita Zabelin <64374751+nikitazabelin@users.noreply.github.com>
2023-07-06 23:33:05 +0300

    Update change-files-extancion.ps1

--- PowerShell/change-files-extancion.ps1
+++ PowerShell/change-files-extancion.ps1
@@ -1,1 +1,1 @@
-Get-ChildItem -Path C:\Users\Administrator\Desktop\test -Filter *.txt | Rename-Item -NewName { $_.name -Replace &#39;.txt$&#39;,&#39;.xml&#39; }
+Get-ChildItem -Path C:\Users\Administrator\Desktop\test -Filter *.txt | Rename-Item -NewName { $_.name -Replace '$.txt''.xml' }
